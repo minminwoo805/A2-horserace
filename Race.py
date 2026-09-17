78be9615d03a3cf8953d7b0f8e2357d89cd74708
minminwoo805 <kmw.2006s@gmail.com>
2026-09-17 18:37:12 +0900

    Add files via upload

--- Race.py
+++ Race.py
@@ -6,7 +6,7 @@ from Dice import *
 
 class Horse :
     def __init__(self, speed, y , image , window):
-        self.x_pos =  40
+        self.x_pos =  0
         self.y_pos = y # x , y 그림의 끝 좌표에 두기
         self.image = image
         self.window = window
